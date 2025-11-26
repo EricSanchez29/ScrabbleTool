849c8c8da5d8f628e639160b2f42e228b8118081
Eric <me@fakeemail.com>
2025-11-26 10:18:00 -0600

    Changed lane definition, will orient them like unit vectors (across and down direction only)

--- ScrabbleGame/ScrabbleBase.cs
+++ ScrabbleGame/ScrabbleBase.cs
@@ -2,5 +2,8 @@ public class ScrabbleBase
 {
     public int X_coordinate { get; set; }
     public int Y_coordinate { get; set; }
+    /// <summary>
+    /// across (true) or down (false) 
+    /// </summary>
     public bool Direction { get; set; }
 }
--- ScrabbleGame/ScrabbleLane.cs
+++ ScrabbleGame/ScrabbleLane.cs
@@ -1,6 +1,11 @@
 public class ScrabbleLane : ScrabbleBase
 {
     public char Tile { get; set; }
+
+    /// <summary>
+    /// 0 index position from left to right (up to down)
+    /// </summary>
+    public int TilePosition { get; set; }
+    
     public int Length { get; set; }
-    public bool Reverse { get; set; }
 }
